47304a1e9736ca34de74a1d06f6d51268633b0d0
David Dierckx <david.dierckx@hotmail.com>
2022-04-07 17:32:16 +0200

    Revert "We zien wel"
    This reverts commit ab94b8421f6691cb42f176b75bac40507655da33.

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("mongodb+srv://daviddierckx:Tv123456789@cluster0.gbnfj.mongodb.net/api-crud", {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
 
             useUnifiedTopology: true
